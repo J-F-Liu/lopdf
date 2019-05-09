d6f12b5bf4ed1bb247bb94348df1ab390520de04
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2019-05-09 13:30:56 -0400

    Add parsing benchmark.

---
+++ benches/parse.rs
@@ -0,0 +1,16 @@
+#![feature(test)]
+use std::fs::File;
+use std::io::{Cursor, Read};
+
+extern crate test;
+use lopdf::Document;
+
+#[bench]
+fn bench_load(b: &mut test::test::Bencher) {
+	let mut buffer = Vec::new();
+	File::open("assets/example.pdf").unwrap().read_to_end(&mut buffer).unwrap();
+
+	b.iter(|| {
+		Document::load_from(Cursor::new(&buffer)).unwrap();
+	})
+}
