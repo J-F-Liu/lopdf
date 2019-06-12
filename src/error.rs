576be50d2e3517f276fb369da98622228bd9a5c6
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2019-06-12 15:43:12 -0400

    Add error descriptions.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum Error {
 	IO(std::io::Error),
@@ -8,6 +10,21 @@ pub enum Error {
 	Parse {offset: usize},
 }
 
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Error::IO(e) => e.fmt(f),
+			Error::Header => write!(f, "Invalid file header"),
+			Error::Trailer => write!(f, "Invalid file trailer"),
+			Error::Xref(e) => write!(f, "Invalid cross-reference table ({})", e),
+			Error::Offset(o) => write!(f, "Invalid file offset: {}", o),
+			Error::Parse{offset, ..} => write!(f, "Invalid object at byte {}", offset),
+		}
+    }
+}
+
+impl std::error::Error for Error {}
+
 #[derive(Debug)]
 pub enum XrefError {
 	Parse,
@@ -15,6 +32,19 @@ pub enum XrefError {
 	PrevStart,
 	StreamStart,
 }
+
+impl fmt::Display for XrefError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			XrefError::Parse => write!(f, "could not parse xref"),
+			XrefError::Start => write!(f, "invalid start value"),
+			XrefError::PrevStart => write!(f, "invalid start value in Prev field"),
+			XrefError::StreamStart => write!(f, "invalid stream start value"),
+		}
+    }
+}
+
+impl std::error::Error for XrefError {}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
