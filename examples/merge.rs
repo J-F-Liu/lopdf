da0dedda8684c2c52778789517aaa03b8b52be9d
Emulator000 <emulator@hotmail.it>
2020-09-25 02:13:17 +0200

    Added multiple PDFs merge into one PDF example

---
+++ examples/merge.rs
@@ -0,0 +1,201 @@
+#[macro_use]
+extern crate lopdf;
+
+use lopdf::content::{Content, Operation};
+use lopdf::{Document, Object, ObjectId, Stream};
+
+pub fn generate_fake_document() -> Document {
+    let mut doc = Document::with_version("1.5");
+    let pages_id = doc.new_object_id();
+    let font_id = doc.add_object(dictionary! {
+        "Type" => "Font",
+        "Subtype" => "Type1",
+        "BaseFont" => "Courier",
+    });
+    let resources_id = doc.add_object(dictionary! {
+        "Font" => dictionary! {
+            "F1" => font_id,
+        },
+    });
+    let content = Content {
+        operations: vec![
+            Operation::new("BT", vec![]),
+            Operation::new("Tf", vec!["F1".into(), 48.into()]),
+            Operation::new("Td", vec![100.into(), 600.into()]),
+            Operation::new("Tj", vec![Object::string_literal("Hello World!")]),
+            Operation::new("ET", vec![]),
+        ],
+    };
+    let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+    let page_id = doc.add_object(dictionary! {
+        "Type" => "Page",
+        "Parent" => pages_id,
+        "Contents" => content_id,
+        "Resources" => resources_id,
+        "MediaBox" => vec![0.into(), 0.into(), 595.into(), 842.into()],
+    });
+    let pages = dictionary! {
+        "Type" => "Pages",
+        "Kids" => vec![page_id.into()],
+        "Count" => 1,
+    };
+    doc.objects.insert(pages_id, Object::Dictionary(pages));
+    let catalog_id = doc.add_object(dictionary! {
+        "Type" => "Catalog",
+        "Pages" => pages_id,
+    });
+    doc.trailer.set("Root", catalog_id);
+
+    doc
+}
+
+fn main() {
+    // Generate a stack of Documents to merge
+    let documents = vec![
+        generate_fake_document(),
+        generate_fake_document(),
+        generate_fake_document(),
+        generate_fake_document(),
+    ];
+
+    // Define a starting max_id (will be used as start index for object_ids)
+    let mut max_id = 1;
+
+    // Collect all Documents Objects grouped by a map
+    let mut documents_objects = Vec::new();
+    for mut document in documents {
+        document.renumber_objects_with(max_id);
+
+        max_id = document.max_id + 1;
+
+        documents_objects.push(document.objects);
+    }
+
+    // Initialize a new empty document
+    let mut document = Document::with_version("1.5");
+
+    // Catalog and Pages are mandatory
+    let mut catalog_object: Option<(ObjectId, Object)> = None;
+    let mut pages_object: Option<(ObjectId, Object)> = None;
+
+    // Iter on all Documents Objects collected before
+    let mut pages: Vec<ObjectId> = Vec::new();
+    for document_objects in documents_objects {
+        // Process all objects except "Page" type
+        for (object_id, object) in document_objects.iter() {
+            // We have to ignore "Page" (as are processed later), "Outlines" and "Outline" objects
+            // All other objects should be collected and inserted into the main Document
+            match object.type_name().unwrap_or("") {
+                "Catalog" => {
+                    // Collect a first "Catalog" object and use it for the future "Pages"
+                    catalog_object = Some((
+                        if let Some((id, _)) = catalog_object {
+                            id
+                        } else {
+                            *object_id
+                        },
+                        object.clone(),
+                    ));
+                }
+                "Pages" => {
+                    // Collect and update a first "Pages" object and use it for the future "Catalog"
+                    // We have also to merge all dictionaries of the old and the new "Pages" object
+                    if let Ok(dictionary) = object.as_dict() {
+                        let mut dictionary = dictionary.clone();
+                        if let Some((_, ref object)) = pages_object {
+                            if let Ok(old_dictionary) = object.as_dict() {
+                                dictionary.extend(old_dictionary);
+                            }
+                        }
+
+                        pages_object = Some((
+                            if let Some((id, _)) = pages_object {
+                                id
+                            } else {
+                                *object_id
+                            },
+                            Object::Dictionary(dictionary),
+                        ));
+                    }
+                }
+                "Page" => {}     // Ignored, processed later and separately
+                "Outlines" => {} // Ignored, not supported yet
+                "Outline" => {}  // Ignored, not supported yet
+                _ => {
+                    document.objects.insert(*object_id, object.clone());
+                }
+            }
+        }
+
+        // If no "Pages" found abort
+        if pages_object.is_none() {
+            println!("Pages root not found.");
+
+            return;
+        }
+
+        // Iter over all "Page" and collect with the parent "Pages" created before
+        for (object_id, object) in document_objects.into_iter() {
+            match object.type_name().unwrap_or("") {
+                "Page" => {
+                    if let Ok(dictionary) = object.as_dict() {
+                        let mut dictionary = dictionary.clone();
+                        dictionary.set("Parent", pages_object.as_ref().unwrap().0);
+
+                        document
+                            .objects
+                            .insert(object_id, Object::Dictionary(dictionary));
+
+                        pages.push(object_id);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    // If no "Catalog" found abort
+    if catalog_object.is_none() {
+        println!("Catalog root not found.");
+
+        return;
+    }
+
+    let catalog_object = catalog_object.unwrap();
+    let pages_object = pages_object.unwrap();
+
+    // Build a new "Pages" with updated fields
+    if let Ok(dictionary) = pages_object.1.as_dict() {
+        let count = pages.len();
+
+        let mut dictionary = dictionary.clone();
+        dictionary.set("Count", count as u32);
+
+        document
+            .objects
+            .insert(pages_object.0, Object::Dictionary(dictionary));
+    }
+
+    // Build a new "Catalog" with updated fields
+    if let Ok(dictionary) = catalog_object.1.as_dict() {
+        let mut dictionary = dictionary.clone();
+        dictionary.set("Pages", pages_object.0);
+        dictionary.remove(b"Outlines"); // Outlines not supported in merged PDFs
+
+        document
+            .objects
+            .insert(catalog_object.0, Object::Dictionary(dictionary));
+    }
+
+    document.trailer.set("Root", catalog_object.0);
+
+    // Update the max internal ID as wasn't updated before due to direct objects insertion
+    document.max_id = document.objects.len() as u32;
+
+    // Reorder all new Document objects
+    document.renumber_objects();
+    document.compress();
+
+    // Save the merged PDF
+    document.save("merged.pdf").unwrap();
+}
