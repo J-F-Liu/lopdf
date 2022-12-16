1c63bc06be22189f2bec993c3f95e056168c78a2
Marty B <mrbm74@gmail.com>
2022-12-16 05:02:23 +0100

    Make some more objects public. (#199)

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub use crate::object::{Dictionary, Object, ObjectId, Stream, StringFormat};
 mod document;
 mod incremental_document;
 mod object_stream;
+pub use object_stream::ObjectStream;
 pub mod xref;
 pub use crate::document::Document;
 pub use crate::incremental_document::IncrementalDocument;
@@ -20,6 +21,7 @@ pub mod content;
 mod creator;
 mod encodings;
 mod error;
+pub use error::XrefError;
 pub mod filters;
 #[cfg(not(feature = "nom_parser"))]
 #[cfg(feature = "pom_parser")]
@@ -30,6 +32,7 @@ mod parser;
 mod parser_aux;
 mod processor;
 mod reader;
+pub use reader::Reader;
 mod writer;
 pub mod xobject;
 pub use error::{Error, Result};
