289e6b9415821475a8e7afe18d4a927df7e45a37
Dmitry Pankratov <dmitry@pankratov.net>
2023-12-26 10:57:22 +0100

    Fixed parsing of the PDFs with incorrect xrefs to indirect objects (#254)

--- src/nom_parser.rs
+++ src/nom_parser.rs
@@ -344,7 +344,7 @@ pub fn indirect_object(
 fn _indirect_object(
     input: &[u8], offset: usize, expected_id: Option<ObjectId>, reader: &Reader,
 ) -> crate::Result<(ObjectId, Object)> {
-    let (i, object_id) = terminated(object_id, pair(tag(b"obj"), space))(input).map_err(|_| Error::Parse { offset })?;
+    let (i, (_, object_id)) = terminated(tuple((space, object_id)), pair(tag(b"obj"), space))(input).map_err(|_| Error::Parse { offset })?;
     if let Some(expected_id) = expected_id {
         if object_id != expected_id {
             return Err(crate::error::Error::ObjectIdMismatch);
