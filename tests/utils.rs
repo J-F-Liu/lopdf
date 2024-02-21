cefb5c0da1236a767f5c1e7239460b8b929f8641
Ech0riginal <samwatkins94@me.com>
2024-02-21 20:48:37 +0000

    fmt

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,17 +1,15 @@
+use lopdf::{Document, IncrementalDocument, Result};
 use std::fmt::Display;
 use std::path::Path;
-use lopdf::{Document, IncrementalDocument, Result};
 
 #[allow(dead_code)]
 pub fn load_document<P>(path: P) -> Result<Document>
-    where P: AsRef<Path> + Display
+where
+    P: AsRef<Path> + Display,
 {
     #[cfg(feature = "async")]
-        let doc = tokio::runtime::Runtime::new()?
-        .block_on(async move {
-            Document::load(&path).await
-                .expect(&*format!("Failed to load {}", path))
-        });
+    let doc = tokio::runtime::Runtime::new()?
+        .block_on(async move { Document::load(&path).await.expect(&*format!("Failed to load {}", path)) });
     #[cfg(not(feature = "async"))]
     let doc = Document::load(path)?;
 
@@ -20,14 +18,15 @@ pub fn load_document<P>(path: P) -> Result<Document>
 
 #[allow(dead_code)]
 pub fn load_incremental_document<P>(path: P) -> Result<IncrementalDocument>
-    where P: AsRef<Path> + Display
+where
+    P: AsRef<Path> + Display,
 {
     #[cfg(feature = "async")]
-        let doc = tokio::runtime::Runtime::new()?
-        .block_on(async move {
-            IncrementalDocument::load(&path).await
-                .expect(&*format!("Failed to load {}", path))
-        });
+    let doc = tokio::runtime::Runtime::new()?.block_on(async move {
+        IncrementalDocument::load(&path)
+            .await
+            .expect(&*format!("Failed to load {}", path))
+    });
     #[cfg(not(feature = "async"))]
     let doc = IncrementalDocument::load(path)?;
 
