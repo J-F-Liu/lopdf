fb538dbd9a9682fb1b831297e7ae85e4319cbf7e
Heinenen <th.m.heinen@gmail.com>
2024-08-12 15:02:42 +0200

    Add error handling to object stream

--- src/object_stream.rs
+++ src/object_stream.rs
@@ -2,10 +2,10 @@
 
 use crate::parser;
 use crate::{Error, Object, ObjectId, Result, Stream};
-use std::cmp::max;
 use std::collections::BTreeMap;
 use std::str::FromStr;
 
+use log::warn;
 #[cfg(feature = "rayon")]
 use rayon::prelude::*;
 
@@ -24,9 +24,12 @@ impl ObjectStream {
             });
         }
 
-        let first_offset = max(0, stream.dict.get(b"First").and_then(Object::as_i64)?) as usize;
-        let _count = max(0, stream.dict.get(b"N").and_then(Object::as_i64)?) as usize;
-
+        let first_offset = stream
+            .dict
+            .get(b"First")
+            .and_then(Object::as_i64)?
+            .try_into()
+            .map_err(|_| Error::Offset(0))?;
         let index_block = stream.content.get(..first_offset).ok_or(Error::Offset(first_offset))?;
 
         let numbers_str = std::str::from_utf8(index_block)?;
@@ -36,10 +39,19 @@ impl ObjectStream {
             .collect();
         let len = numbers.len() / 2 * 2; // Ensure only pairs.
 
+        let n = stream.dict.get(b"N").and_then(Object::as_i64)?;
+        if numbers.len().try_into().ok() != n.checked_mul(2) {
+            warn!("object stream: the object stream dictionary specifies a wrong number of objects")
+        }
+
         let chunks_filter_map = |chunk: &[_]| {
             let id = chunk[0]?;
             let offset = first_offset + chunk[1]? as usize;
 
+            if offset >= stream.content.len() {
+                warn!("out-of-bounds offset in object stream");
+                return None;
+            }
             let object = parser::direct_object(&stream.content[offset..])?;
 
             Some(((id, 0), object))
