ea0f3a4c9c033ffe7c2479d4492144dcbc5b49d8
Daniel Kaluza <daniel.kaluza.dk@gmail.com>
2024-09-14 19:55:11 +0200

    Implement ToUnicode for variadic len encodings in nom parser only

--- src/cmap_section.rs
+++ src/cmap_section.rs
@@ -1,19 +1,22 @@
 /*
 ToUnicode CMaps are special CMaps and thus can be parsed simpler. Assumptions:
 - /CMapType is always 2
-- codespace range always from <0000> to <ffff>
 - only bfchar and bfrange sections allowed
 - no glyph names as target allowed, only hex strings
-- source character codes always 2 bytes
 - target encoded in UTF16-BE
  */
 
 pub(crate) type ArrayOfTargetStrings = Vec<Vec<u16>>;
-pub(crate) type SourceRangeMapping = ((u16, u16), ArrayOfTargetStrings);
-pub(crate) type SourceCharMapping = (u16, Vec<u16>);
+
+// According to pdf documentation source codes can be of various byte length but they
+// should be smaller than integer
+pub(crate) type SourceCode = u32;
+pub(crate) type CodeLen = u8;
+pub(crate) type SourceRangeMapping = ((SourceCode, SourceCode, CodeLen), ArrayOfTargetStrings);
+pub(crate) type SourceCharMapping = ((SourceCode, CodeLen), Vec<u16>);
 #[derive(Debug, PartialEq)]
 pub enum CMapSection {
-    CsRange(Vec<(u16, u16)>),
+    CsRange(Vec<(SourceCode, SourceCode, CodeLen)>),
     BfChar(Vec<SourceCharMapping>),
     BfRange(Vec<SourceRangeMapping>),
 }
